51cd3cf52fc1f63f05e9fa4f3978f619758d16e3
John Lathouwers <john.lathouwers@oracle.com>
2022-08-01 10:30:33 +0100

    changes the image; fixes docker runner cmd

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   compartment_ocid = var.compartment_ocid != "" ? var.compartment_ocid : var.tenancy_ocid
   user_ocid        = var.user_ocid != "" ? var.user_ocid : var.current_user_ocid
 
-  compute_image     = "Oracle Linux Cloud Developer"
+  compute_image     = "Oracle Linux"
   compute_image_ver = "8"
   compute_user      = "opc"
   compute_shape     = "VM.Standard.A1.Flex"
